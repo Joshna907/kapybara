bd1a18eb74e32cecbad5b766f28f63076bd8a6f3
Joshna Waikar <joshnawaikar@gmail.com>
2025-11-09 06:01:51 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
   typescript: {
-        ignoreBuildErrors: true,
-      },
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
