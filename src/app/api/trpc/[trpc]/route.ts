d136ab6a1f4b201f0e763876963255de44ce7510
Joshna Waikar <joshnawaikar@gmail.com>
2025-11-09 06:44:01 -0800

    Update route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/server/routers/_app";
-import { createTRPCContext } from "@/server/trpc/routers/context";
+import { createContext } from "@/server/trpc/routers/context";
 
 
 const handler = (req: Request) =>
